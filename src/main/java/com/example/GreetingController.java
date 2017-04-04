627a88436e75326693a8dfc2d0e59b45d98eb017
anh student <a.t.tran@uqconnect.edu.au>
2017-04-04 10:59:46 +0200

    change controller

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
     private static final String template = "This is Anh's first web app, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/helloWorld")
+    @RequestMapping("/")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
