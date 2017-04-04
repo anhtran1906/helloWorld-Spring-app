4718c2dcb9e3b84d44a10f5ccab74a8d8f1592be
anh student <a.t.tran@uqconnect.edu.au>
2017-04-04 10:54:56 +0200

    change

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -5,15 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
-@Controller
+//@Controller
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
+    /*@RequestMapping("/")
     @ResponseBody
     String home() {
       return "Hello World!";
-    }
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
